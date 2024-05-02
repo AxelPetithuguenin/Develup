0e8b26122340515bfbaf0bf135de452acae8e475
EthanDeL <ethandelbos@orange.fr>
2024-05-02 17:50:48 +0200

    Update

--- FrontOffice/public/assets/js/scroll.js
+++ FrontOffice/public/assets/js/scroll.js
@@ -1,181 +1,175 @@
 //  SCROLLREVEAL VARIABLES 
 const sr = ScrollReveal();
 
-// ACCUEIL PAGE //
-    sr.reveal('.landing-page-text-container', {
-   duration: 1000,
-     origin: 'left',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
+    // ACCUEIL PAGE //
+sr.reveal('.landing-page-text-container', {
+    duration: 1000,
+    origin: 'left',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+});
+sr.reveal('.landing-page-image-container', {
+    duration: 1000,
+    origin: 'right',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+});
+sr.reveal('.social-network-content', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+});
+sr.reveal('.nbre-inscrits-content', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+});
+sr.reveal('.scroll', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 600,
+    easing: 'ease-in-out',
+});
+sr.reveal('.message', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
  });
- sr.reveal('.landing-page-image-container', {
-     duration: 1000,
-     origin: 'right',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
- });
- sr.reveal('.social-network-content', {
-     duration: 1000,
-     origin: 'bottom',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
- });
- sr.reveal('.nbre-inscrits-content', {
-     duration: 1000,
-     origin: 'bottom',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
- });
- sr.reveal('.scroll', {
-     duration: 1000,
-     origin: 'bottom',
-     distance: '50px',
-     delay: 600,
-     easing: 'ease-in-out',
- });
- sr.reveal('.message', {
-     duration: 1000,
-     origin: 'bottom',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
- });
- sr.reveal('.slider-temoignage#slider-temoignage', {
-     duration: 1000,
-     origin: 'left',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
- });
+sr.reveal('.slider-temoignage#slider-temoignage', {
+    duration: 1000,
+    origin: 'left',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+});
 
+    //  TEMOIGNAGE PAGE // 
+sr.reveal('.card-temoignage-page', {
+    duration: 1000,
+    origin: 'bottom', 
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+    interval: 200 
+});
 
-//  TEMOIGNAGE PAGE // 
- sr.reveal('.card-temoignage-page', {
-     duration: 1000,
-     origin: 'bottom', // ou toute autre origine souhaitée
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
-     interval: 200 // délai entre chaque élément
- });
-
-
-
- // PERSONNAL TEMOIGNAGE //
- sr.reveal('.header-personnal-temoignage-page-content', {
-     duration: 1000,
-     origin: 'left', 
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out'
- });
- sr.reveal('.personnal-temoignage-text', {
-     duration: 1000,
-     origin: 'bottom', 
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out'
- });
-
+    // PERSONNAL TEMOIGNAGE //
+sr.reveal('.header-personnal-temoignage-page-content', {
+    duration: 1000,
+    origin: 'left', 
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
+sr.reveal('.personnal-temoignage-text', {
+    duration: 1000,
+    origin: 'bottom', 
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
 
  // DON MOELLE OSSEUSE // 
     // C EST QUOI // 
- sr.reveal('.paragraphe-text', {
-     duration: 1000,
-     origin: 'bottom',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out'
- });
- sr.reveal('.personnal-temoignage-text .paragraphe-text', {
-     duration: 1000,
-     origin: 'bottom',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
-     interval: 200 
- });
- sr.reveal('.video-container', {
-     duration: 1000,
-     origin: 'bottom',
-     distance: '50px',
-     delay: 500,
-     easing: 'ease-in-out',
-     interval: 200 
- });
- sr.reveal('.simple-card', {
-     duration: 1000,
+sr.reveal('.paragraphe-text', {
+    duration: 1000,
     origin: 'bottom',
-     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out',
-     interval: 200
- });
- sr.reveal('.text-link-container', {
-     duration: 1000,
-     origin: 'bottom',
     distance: '50px',
-     delay: 300,
-     easing: 'ease-in-out'
- });
-
+    delay: 300,
+    easing: 'ease-in-out'
+});
+sr.reveal('.personnal-temoignage-text .paragraphe-text', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+    interval: 200 
+});
+sr.reveal('.video-container', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 500,
+    easing: 'ease-in-out',
+    interval: 200 
+});
+sr.reveal('.simple-card', {
+    duration: 1000,
+   origin: 'bottom',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+    interval: 200
+});
+sr.reveal('.text-link-container', {
+    duration: 1000,
+    origin: 'bottom',
+   distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
 
  // PRESENTATION ASSOCIATION //
  sr.reveal('.edito-president-container img', {
-     duration: 1000,
-     origin: 'left', 
-     distance: '50px', 
- });
- sr.reveal('.middle-title', {
-     duration: 1000,
-     origin: 'top',
-     distance: '30px', 
-     delay: 500 
- });
- sr.reveal('.text-edito-container p', {
-     duration: 1000,
-    origin: 'right', 
-     distance: '50px',
-     delay: 1000 
- });
- sr.reveal('.presentation-box', {
-     duration: 1000, 
-     origin: 'bottom', 
-     distance: '50px', 
-     interval: 200 
- });
- sr.reveal('section.container-page p.text', {
-     duration: 1000, 
-     origin: 'bottom',
-     distance: '30px', 
+    duration: 1000,
+    origin: 'left', 
+    distance: '50px', 
+});
+sr.reveal('.middle-title', {
+    duration: 1000,
+    origin: 'top',
+    distance: '30px', 
     delay: 500 
- });
+});
+sr.reveal('.text-edito-container p', {
+    duration: 1000,
+    origin: 'right', 
+    distance: '50px',
+    delay: 1000 
+});
+sr.reveal('.presentation-box', {
+    duration: 1000, 
+    origin: 'bottom', 
+    distance: '50px', 
+    interval: 200 
+});
+sr.reveal('section.container-page p.text', {
+    duration: 1000, 
+    origin: 'bottom',
+    distance: '30px', 
+   delay: 500 
+});
 
+     // PARTENAIRES //
+sr.reveal('.card-box-image', {
+    delay: 200,
+    duration: 1000,
+    interval: 300,
+    origin: 'bottom',
+    distance: '20px',
+    easing: 'ease-in-out',
+});
 
- // PARTENAIRES //
- sr.reveal('.card-box-image', {
-     delay: 200,
-     duration: 1000,
-     interval: 300,
-     origin: 'bottom',
-     distance: '20px',
-     easing: 'ease-in-out',
- });
+// ACTUALITE //
+sr.reveal('.simple-card-actualite', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 300,
+    easing: 'ease-in-out',
+    interval: 200 
+});
 
- // ACTUALITE //
-    
-    sr.reveal('.simple-card-actualite', {
-        duration: 1000,
-        origin: 'bottom',
-        distance: '50px',
-        delay: 300,
-        easing: 'ease-in-out',
-        interval: 200 // délai entre chaque élément
-    });
 // PERSONNAL ACTUALITE //
 sr.reveal('.header-personnal-actualite', {
     duration: 1000,
